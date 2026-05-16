136495cd2873ab20b7398c563c5fc466ab09e666
DhruvPokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2026-05-16 21:31:40 +0530

    create get endpoint to fetch a certain event from slug

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -63,3 +63,6 @@ export async function GET() {
         return NextResponse.json({message: 'Failed to fetch events', error: error instanceof Error ? error.message : 'Unknown error'}, { status: 500 });
     }
 }
+
+
+
